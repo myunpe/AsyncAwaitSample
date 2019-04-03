d66c8a418d01d68e9e1df6ae0acbd810c292e7a7
myunpe <myunpe2@gmail.com>
2019-04-03 23:54:18 +0900

    Awaitar作成中

---
+++ Sample/Assets/SampleAwaitar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+
+namespace Lib
+{
+    public interface IAwaitar<T> : ICriticalNotifyCompletion
+    {
+        bool IsCompleted{ get; }
+        void OnCompleted(Action continuation);
+        T GetResult();
+    }
+
+    public class SampleAwaitar<T>
+    {
+    }
+    
+    
+    
+}
