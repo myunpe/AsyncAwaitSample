cd1b51b1717d71ae011cd265d301948b73f0aab0
myunpe <myunpe2@gmail.com>
2019-04-21 21:14:09 +0900

    コールバックパターン追加

---
+++ Sample/Assets/CallbackCoroutine/CallbackCoroutine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AwaiterExtensions;
+using UnityEngine;
+
+public class CallbackCoroutine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    async void Start()
+    {
+        StartCoroutine(LoadCoroutine("cube", o => Instantiate(o)));
+        
+        var cube = await LoadAsync("cube");
+        Instantiate(cube);
+    }
+
+    IEnumerator LoadCoroutine(string path, Action<GameObject> callback)
+    {
+        var operation = Resources.LoadAsync<GameObject>(path);
+        while (!operation.isDone)
+        {
+            yield return null;
+        }
+
+        var cube = operation.asset as GameObject;
+        callback.Invoke(cube);
+    }
+
+    async Task<GameObject> LoadAsync(string path)
+    {
+        return await Resources.LoadAsync<GameObject>(path) as GameObject;
+    }
+}
