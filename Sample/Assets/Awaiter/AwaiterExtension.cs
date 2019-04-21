552e20cdf157d1e2b1408fbbb037bcaf67db6234
myunpe <myunpe2@gmail.com>
2019-04-22 08:58:56 +0900

    awaiter追加

--- Sample/Assets/Awaiter/AwaiterExtension.cs
+++ Sample/Assets/Awaiter/AwaiterExtension.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Networking;
 using Object = UnityEngine.Object;
 
 namespace AwaiterExtensions
@@ -24,6 +25,39 @@ namespace AwaiterExtensions
             };
             operation.completed += callback;
 
+            return tcs.Task.GetAwaiter();
+        }
+
+        public static TaskAwaiter<AsyncOperation> GetAwaiter(this AsyncOperation operation)
+        {
+            var tcs = new TaskCompletionSource<AsyncOperation>();
+
+            Action<AsyncOperation> callback = null;
+            callback = (op) =>
+            {
+                op.completed -= callback;
+                tcs.SetResult(op);
+            };
+
+            operation.completed += callback;
+            
+            return tcs.Task.GetAwaiter();
+        }
+        
+        public static TaskAwaiter<UnityWebRequest> GetAwaiter(this UnityWebRequestAsyncOperation operation)
+        {
+            var tcs = new TaskCompletionSource<UnityWebRequest>();
+
+            Action<AsyncOperation> callback = null;
+            callback = (op) =>
+            {
+                var request = op as UnityWebRequestAsyncOperation;
+                op.completed -= callback;
+                tcs.SetResult(request.webRequest);
+            };
+
+            operation.completed += callback;
+            
             return tcs.Task.GetAwaiter();
         }
     }
--- Sample/Assets/NestCoroutine/NestedCoroutine.cs
+++ Sample/Assets/NestCoroutine/NestedCoroutine.cs
@@ -1,18 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using AwaiterExtensions;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class NestedCoroutine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    async void Start()
     {
-        
+        StartCoroutine(RequestCoroutine(() =>
+        {
+            StartCoroutine(LoadCoroutine("cube", (cube) =>
+            {
+                Instantiate(cube);
+            }));
+        }));
+
+        var webRequest = await UnityWebRequest.Get("http://google.co.jp").SendWebRequest();
+        Debug.Log($"downloadtext={webRequest.downloadHandler.text}"); 
+        var cubeObject = await Resources.LoadAsync<GameObject>("cube");
+        Instantiate(cubeObject);
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+    IEnumerator RequestCoroutine(Action callback)
     {
+        var request = UnityWebRequest.Get("http://google.co.jp").SendWebRequest();
+        while (!request.isDone)
+        {
+            yield return null;
+        }
+
+        var text = request.webRequest.downloadHandler.text;
+        Debug.Log($"text={text}");
         
+        callback.Invoke();
+    }
+    
+    IEnumerator LoadCoroutine(string path, Action<GameObject> callback)
+    {
+        var operation = Resources.LoadAsync<GameObject>(path);
+        while (!operation.isDone)
+        {
+            yield return null;
+        }
+
+        var cube = operation.asset as GameObject;
+        callback.Invoke(cube);
     }
 }
