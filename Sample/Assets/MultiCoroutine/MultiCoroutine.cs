9f7e9bfb601a315bafea8ef0cd062db650616015
myunpe <myunpe2@gmail.com>
2019-04-21 21:37:28 +0900

    複数同時に動かす

---
+++ Sample/Assets/MultiCoroutine/MultiCoroutine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AwaiterExtensions;
+using UnityEngine;
+
+public class MultiCoroutine : MonoBehaviour
+{
+
+    async void Start()
+    {
+        StartCoroutine(LoadCoroutine("cube", o => Instantiate(o)));
+        StartCoroutine(LoadCoroutine("cube", o => Instantiate(o)));
+
+        var cubes = await Task.WhenAll(LoadAsync("cube"), LoadAsync("cube"));
+        Instantiate(cubes[0]);
+        Instantiate(cubes[1]);
+    }
+
+    IEnumerator LoadCoroutine(string path, Action<GameObject> callback)
+    {
+        var operation = Resources.LoadAsync<GameObject>(path);
+        while (!operation.isDone)
+        {
+            yield return null;
+        }
+
+        var cube = operation.asset as GameObject;
+        callback.Invoke(cube);
+    }
+
+    async Task<GameObject> LoadAsync(string path)
+    {
+        return await Resources.LoadAsync<GameObject>(path) as GameObject;
+    }
+}
